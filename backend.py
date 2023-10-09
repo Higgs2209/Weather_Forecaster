b9dd401eea04ebc31309eadf55f290a44243a723
connor <connord79@gmail.com>
2023-10-09 14:08:59 +1100

    Error handling

--- backend.py
+++ backend.py
@@ -1,21 +1,20 @@
 from constants import api_key
 import requests
 
-def get_data(place, forecast_days=None, kind=None):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={api_key}"
     response = requests.get(url)
+    print(response)
     data = response.json()
+    print(data)
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
+    print(filtered_data)
     return filtered_data
 
 
-if __name__=="__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
--- main.py
+++ main.py
@@ -10,12 +10,25 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-data = get_data(place, days, option)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
 
+    except KeyError:
+        st.text("Error: Please enter a valid place name")
+        print("error")
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-
-st.plotly_chart(figure)
 
